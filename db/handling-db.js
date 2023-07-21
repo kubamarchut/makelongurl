262e3a58c1ea8451577984036433e26bf23d0c87
kuba marchut <kuba.marchut@gmail.com>
2023-07-21 12:23:37 +0200

    fixing problems with db connection

--- db/handling-db.js
+++ db/handling-db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbUrl = "mongodb://127.0.0.1:27017/makelongurl-db"
+const dbUrl = "mongodb://mongodb:27017/makelongurl-db"
 
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
